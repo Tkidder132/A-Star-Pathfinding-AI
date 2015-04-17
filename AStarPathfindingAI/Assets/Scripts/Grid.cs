33fc13c5c728ba75c2814f80ee37b3c216f254cd
Tristan <tkidder132@gmail.com>
2015-04-17 01:25:16 -0500

    Pathfinding AI
    added pathfinding AI script

--- AStarPathfindingAI/Assets/Scripts/Grid.cs
+++ AStarPathfindingAI/Assets/Scripts/Grid.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class Grid : MonoBehaviour
 {
@@ -18,6 +19,32 @@ public class Grid : MonoBehaviour
 		gridSizeX = Mathf.RoundToInt(gridWorldSize.x / nodeDiameter);
 		gridSizeY = Mathf.RoundToInt(gridWorldSize.y / nodeDiameter);
 		CreateGrid();
+	}
+
+	public List<Node> GetNeighbors(Node node)
+	{
+		List<Node> neighbors = new List<Node>();
+
+		for( int x = -1; x <= 1; x++ )
+		{
+			for( int y = -1; y <= 1; y++ )
+			{
+				if( x == 0 && y == 0 )
+				{
+					continue;
+				}
+
+				int checkX = node.gridX + x;
+				int checkY = node.gridY + y;
+
+				if( checkX >= 0 && checkX < gridSizeX && checkY >= 0 && checkY < gridSizeY )
+				{
+					neighbors.Add(grid[checkX, checkY]);
+				}
+			}
+		}
+
+		return neighbors;
 	}
 
 	public Node NodeFromWorldPoint(Vector3 worldPosition)
@@ -45,7 +72,7 @@ public class Grid : MonoBehaviour
 			{
 				Vector3 worldPoint = worldBottomLeft + Vector3.right * (x * nodeDiameter + nodeRadius) + Vector3.forward * (y * nodeDiameter + nodeRadius);
 				bool walkable = !(Physics.CheckSphere(worldPoint, nodeRadius, unwalkableMask));
-				grid[x,y] = new Node(walkable, worldPoint);
+				grid[x,y] = new Node(walkable, worldPoint, x, y);
 			}
 		}
 	}
